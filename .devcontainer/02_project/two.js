970e8646395e46278092bfeb0c87ff063f5e26e3
Ankit Kumar Pal <148264717+Developer-AnkitKumar@users.noreply.github.com>
2024-08-13 05:19:10 +0000

    project01

---
+++ .devcontainer/02_project/two.js
@@ -0,0 +1,11 @@
+//Generate a random number for multiple color
+
+const randomNumber = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#'
+    for(let i = 0; i<6; i++){
+
+    }
+};
+
+console.log = (math.randomNumber() * 16);
