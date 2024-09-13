5d542a27499ba73135c070ba23e8a96319645e51
Ankit Kumar Pal <148264717+Developer-AnkitKumar@users.noreply.github.com>
2024-09-13 21:20:15 +0000

    .devcontainer

---
+++ .devcontainer/05_controlFlow/condition.js
@@ -0,0 +1,44 @@
+// If condition
+
+if(true){
+    // console.log("Yes is it right for this condition");
+    
+}
+
+const isLoggedIn = true
+
+if(isLoggedIn){
+    // console.log("isLoggedIn user");
+    
+}
+// console.log("isLoggrdIn user is true");
+
+const todayTemperatureIs = 42
+
+if(todayTemperatureIs > 50){
+    // console.log("temperature is grater then 50");
+    
+}else{
+// console.log("temperature is less then 50");
+}
+
+const score = 1000
+
+if(score < 900){
+    // console.log("score is greater then 1000");
+    
+}else if(score > 900){
+    // console.log("score is less then 1000");
+    
+}else{
+    // console.log("score is also less then 1000");
+    
+}
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard && 2 == 2){ // tdi yhi 3 kr dete one side to kuch bhi print nhi hota
+    console.log("Allow to buy this course"); 
+}
+
---
+++ .devcontainer/05_controlFlow/truthe_falshe.js
@@ -0,0 +1,18 @@
+// Truthe value
+
+// const userEmail = "apal66860@gmail.com"
+const userEmail = []
+
+if(userEmail){
+    console.log("got user email:");
+    
+}else{
+    console.log("don't have user email:");
+    
+}
+
+//false value........
+//false, 0, -0, bigInt 0n, "", null, undefine, NaN.
+
+//truthe value......
+//"0", 'false, " ", [], {}, function(){}, 
