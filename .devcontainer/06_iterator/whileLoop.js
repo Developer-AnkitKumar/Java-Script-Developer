6852d4b07119c2db4f2e6ab72492879f70d3b057
Ankit Kumar Pal <148264717+Developer-AnkitKumar@users.noreply.github.com>
2024-09-16 18:15:50 +0000

    .devcontainer

---
+++ .devcontainer/06_iterator/whileLoop.js
@@ -0,0 +1,25 @@
+// while loop condition
+
+let index = 0
+while (index <= 10) {
+    // console.log(`index value of ${index}`);
+    // index = index + 1;
+    index = index + 2;
+}
+
+let countArr =["Ankit kumar", "Kavita", "Batman", "Captan"] // stroe data in array
+
+let arr = 0 // initialize value 
+while (arr < countArr.length) {
+    // console.log(`index value is ${countArr[arr]}`);
+    // arr = arr + 1
+    arr += 1
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    // score += 1
+    score++;  
+    
+} while (score <= 10);
