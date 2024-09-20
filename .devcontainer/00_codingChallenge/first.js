3672ab22927043366d68b5e301ba348c0aab7a2d
Ankit Kumar Pal <148264717+Developer-AnkitKumar@users.noreply.github.com>
2024-09-20 07:05:25 +0000

    .devcontainer

---
+++ .devcontainer/00_codingChallenge/first.js
@@ -0,0 +1,47 @@
+// Boolean condition
+// True(1) & False(0):
+
+// console.log(1 + true); // Coditional Boolean for True(1).
+// console.log(1 + false); // Same for False(0).
+// // Data type-------
+// // string dataType.....
+// console.log(1 + "3"); // ydi stirng end me lga ho to last ko string  value return hoti hai
+// console.log("1" + 3);  // ydi starting me string lga to sbhi ko string me value return kraygi.
+// console.log("1" + "3");
+// console.log(1 + 2 + "3"); // isme starting me value add hongi fir end me sting add ho jayegi.
+// console.log(1 + "2" + "3");
+// console.log("1" + 2 + 3);
+
+console.log("" + "");
+
+let myVariable = 'Bomb'
+myVariable = 'ANkit kumar'
+myVar = "Kavita Pal"
+
+// console.log(myVariable);
+// console.log(myVar);
+// console.log(typeof(myVariable));
+// console.log(myVariable);
+
+// console.log(typeof eval("3 + 5")); // Eval = evaluate this peoblem
+
+// var x
+// console.log(x); // valur is undefine
+
+// console.log(x);
+// let x = 42
+
+// console.log(y);
+// const y = 42 // Reference error
+
+// const MY_ARRAY = ["HTML", "CSS", "CPP"];
+// // MY_ARRAY.push("JAVASCRIPT");
+// MY_ARRAY.pop("CPP");
+// console.log(MY_ARRAY); 
+
+// const MY_OBJECT = { key: "value" };
+// MY_OBJECT.key = "otherValue";
+// console.log(MY_OBJECT);
+
+
+
